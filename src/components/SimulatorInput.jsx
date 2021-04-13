4925bcc0a95b94f95bc066816ff675b57ff2dbb3
eloy98g <eloygomez132@gmail.com>
2021-04-13 13:37:38 +0200

    all options added to simulator input

--- src/components/SimulatorInput.jsx
+++ src/components/SimulatorInput.jsx
@@ -28,16 +28,43 @@ const SimulatorInput = () => {
             <input type="text" className="sim-input" />
           </div>
         </div>
-        <div className="sim-separator" style={{marginBottom: '20px'}} />
+        <div className="sim-separator" style={{ marginBottom: '20px' }} />
         <div className="content-row">
           <p className="sim-title">Tipo de reemplazo</p>
-          <ItemSelection name="reemplazo" items={['LRU', 'FIFO', 'ALEATORIO']} />
+          <ItemSelection
+            name="reemplazo"
+            items={['LRU', 'FIFO', 'ALEATORIO']}
+          />
         </div>
-        <div className="content-row" style={{marginTop: '20px'}}>
+        <div className="content-row" style={{ marginTop: '20px' }}>
           <p className="sim-title">Tipo de mapeo</p>
-          <ItemSelection name="mapeo" items={['Directo', 'Por conjuntos', 'Asociativo']} />
+          <ItemSelection
+            name="mapeo"
+            items={['Directo', 'Por conjuntos', 'Asociativo']}
+          />
         </div>
-        <div className="sim-separator" style={{marginTop: '20px'}} />
+        <div className="sim-separator" style={{ marginTop: '20px' }} />
+        <b style={{ marginTop: '20px' }}>Avanzadas</b>
+        <div style={{ marginTop: '20px' }} className="content-row">
+          <p className="sim-title" style={{ marginRight: '10px' }}>
+            Tipo de caché
+          </p>
+          <ItemSelection name="tipo" items={['Unificada', 'Mixta']} />
+          <p
+            className="sim-title"
+            style={{ marginRight: '10px', marginLeft: '10px' }}
+          >
+            Política escritura
+          </p>
+          <ItemSelection name="politica" items={['Directa', 'Al retorno']} />
+        </div>
+        <div style={{ marginTop: '20px' }} className="content-row">
+          <p className="sim-title">Políticas de fallo de escritura</p>
+          <ItemSelection
+            name="fallo"
+            items={['Ubicar en escritura', 'No ubicar en escritura']}
+          />
+        </div>
       </div>
     </div>
   );
